afee633857d0bdc5d3dbbc38239e72f2ca195f26
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-10 15:53:49 +0200

    creato il template del progetto e inserite le news nello state (api)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    notizie : []
+  }
+
+  componentDidMount(){
+    this.consulareNotizie();
+  }
+  consulareNotizie = () => {
+    let url = `https://newsapi.org/v2/everything?q=bitcoin&from=2018-09-10&sortBy=publishedAt&apiKey=eb10258121314635876caad77ca78b61`;
+    // console.log(url)
+
+    fetch(url)
+      .then(risposta => {
+        return risposta.json();
+      })
+      .then(notizie => {
+        this.setState({
+          notizie: notizie.articles
+        })
+      })
+  }
+
+
+
   render() {
     return (
       <div>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+const Header = (props) => {
+  return (
+    <div>
+      <Header 
+        titolo = "Notizie"
+      />
+    </div>
+  )
+}
+
+export default Header;
