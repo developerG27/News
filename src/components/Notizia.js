70c2da23b87d34ede8489b3e6c045b91e7fba59c
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-11 14:47:27 +0200

    identificando gli articolo

--- src/components/Notizia.js
+++ src/components/Notizia.js
@@ -1,13 +1,20 @@
 import React from 'react';
 const Notizia = (props) => {
-  const {urlToImage, url, title, description, source} = props.notizia;
+  const {urlToImage, url, title, description, source, author, content } = props.notizia;
 
   console.log(urlToImage);
 
 
   return(
-    <div>
-      <p>Da notizia</p>
+    <div class="articolo">
+      <div>
+        <img src={urlToImage} alt={title} />
+        <p class="titolo">{title}</p>
+        <p class="author"> {author}</p>
+        <p class="description">{description}</p>
+        <a href={url}> <p>Questo è il link</p> </a>
+        <p class="content">{content}</p>
+      </div>
     </div>
   )  
 }
